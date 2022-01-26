7bca4c4803031e42d092f668d1d1b24b04c297b2
SpeedSterKawaii <72403623+SpeedSterKawaii@users.noreply.github.com>
2022-01-26 10:07:20 -0500

    Update KeySys.cpp

--- KeySys.cpp
+++ KeySys.cpp
@@ -2,3 +2,25 @@
 #include <string>
 #include <Windows.h>
 #include <iostream>
+
+string key = "my_key_bussin";//replace with key
+string keywatf = ""; //USERS JOB to REPLACE THIS with 'key'
+
+int start()
+{
+  printf("Ayo Im gonna check your key alright?")
+    Sleep(1000);
+  if (key.find(keywatf) != std::string::npos) {
+    std::cout << "OMG congrats ur a retard!" << '\n';
+    
+    else
+    {
+      printf("idiot thats not the key haha!\n");//ur a idiot and ye.
+    }
+    printf("now im gonna exit cuz u skid");
+    printf(" goodbye in 3 seconds");
+    Sleep(3000);
+    exit(0);
+}
+    return NULL;
+}
